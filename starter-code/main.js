46b36a80f7604f1ffcc8cac0af7a5432463e09d3
Mechwarrior21 <kevinkkan@rogers.com>
2017-01-12 17:15:33 +0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,11 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+	window.alert("You found a match!")
+} else if (cardThree === cardFour) {
+	window.alert("You found a match!")
+} else {
+	window.alert("Sorry, try again.")
+}
