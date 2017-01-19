8687c0dc33191010b7c20f7c6f04908dae9c13bb
Mechwarrior21 <kevinkkan@rogers.com>
2017-01-19 16:28:57 +0800

    created for loop

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,26 @@
+console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
+
+if ("king" = "king") {
 	window.alert("You found a match!")
-} else if (cardThree === cardFour) {
+}; 
+else if ("queen"= "queen") {
 	window.alert("You found a match!")
-} else {
+}; 
+else {
 	window.alert("Sorry, try again.")
+};
+
+var createCards = function() {	
+	var gameboard = document.getElementById('game-board'); {
+		for (var 1 = 0; i < 4; i++)
+		var newCard = document.createElement('div');
+		newCard.class.Name = "card";
+		document.getElementById("game-board")[1].appendChild (newCard);
+	}
+	
 }
